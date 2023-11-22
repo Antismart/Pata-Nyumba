b7acd748d2012b9dae0aaa4c5850f463ec5e3782
Moses <timbwamoses83@gmail.com>
2023-11-22 15:48:45 +0300

    create page sign in functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,12 +7,12 @@ export const signup = async (req, res, next) => {
  const hashedPassword = bcryptjs.hashSync(password, 10);
  const newUser = new User({ username, email, password: hashedPassword });
  try {
-    await newUser.save()
-    res.status(201).json("User created successfully")
+    await newUser.save();
+    res.status(201).json("User created successfully");
  } catch (error) {
     next(error);
  } 
-}
+};
 
 export const signin = async (req, res, next) => {
    const { email, password } = req.body;
@@ -23,7 +23,7 @@ export const signin = async (req, res, next) => {
       if (!validPassword) return next(errorHandler(401, 'Invalid credentials!'));
       const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = validUser._doc;
-      res.cookie('access_token', token, {httpOnly: true})
+      res.cookie('access_token', token, { httpOnly: true})
       .status(200)
       .json(rest);
    } catch (error) {

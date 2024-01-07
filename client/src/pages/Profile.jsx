d48414ce1d7def3e31ef27ffef7a995a16dfb7fa
Antismart <timbwamoses83@gmail.com>
2024-01-07 16:10:03 +0300

    complete show user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -254,11 +254,14 @@ export default function Profile() {
         Show Listing
       </button>
       <p className=' text-red-700 mt-5'>
-        {showListingsError ? 'Error showing listings' : ''}
+        {showListingsError ? 'Error showing listings!' : ''}
       </p>
       
-      {userListings && userListings.length > 0 &&
-      userListings.map((listing) => 
+      {userListings && 
+      userListings.length > 0 &&
+      <div className='flex flex-col gap-4'>
+        <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+      {userListings.map((listing) => (
       <div key={listing._id} className=' border rounded-lg 
       p-3 flex justify-between items-center'>
         <Link to={`/listing/${listing._id}`}
@@ -273,11 +276,13 @@ export default function Profile() {
           <p >{listing.name}</p>
         </Link>
 
-        <div className=''>
-
+        <div className=' flex flex-col item-center'>
+          <button className=' text-red-700 uppercase'>Delete</button>
+          <button className=' text-green-700 uppercase'>Edit</button>
         </div>
       </div>
-    )}
+      ))}
+      </div> }
     </div>
-  ) 
+  ); 
 }
